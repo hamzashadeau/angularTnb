c384569c19bf7a7017afae1a9018a514395056c2
hamza <shadeau99@gmail.com>
2020-04-06 03:05:30 +0100

    Revert "Auto stash before merge of "master" and "origin/master""
    This reverts commit 57261a4fd5f0781ee61dd00496f9fb05331ee43a.

--- src/app/controller/model/achat.model.ts
+++ src/app/controller/model/achat.model.ts
@@ -2,9 +2,11 @@ import {Redevable} from './redevable.model';
 import {Terrain} from './terrain.model';
 
 export class Achat {
+  public id: number;
   public reference: string;
   public oldredevable: Redevable;
   public newRedevable: Redevable;
   public terrain: Terrain;
   public prix: number;
+
 }
